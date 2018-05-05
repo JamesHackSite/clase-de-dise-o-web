780062441468f74cf9610d10486bd48f2ab401e8
Jimmy HackSite <ljoo_naruto25@hotmail.com>
2018-05-04 21:29:24 -0500

    subiendo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './comp';
+
+class Comp extends Component {
+  render()
+    return ()
+    );
+  }
+}
+
+export default Comp;
---
+++ src/comp/nativ/nativ.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import import './src/comp/nativ/nativ.css';
+
+
+
+class nativ extends Component
+    render() {
+      return (
+        <div class="fixedHeaderContainer">
+<div class="headerWrapper wrapper">
+<header>
+<a href="/react-native/">
+<img class="logo" src="/react-native/img/header_logo.png">
+<h2 class="headerTitle">React Native</h2></a><a href="/react-native/versions.html">
+<h3>0.55</h3></a>
+<div class="navigationWrapper navigationSlider">
+<nav class="slidingNav"><ul class="nav-site nav-site-internal">
+<li class="siteNavGroupActive siteNavItemActive">
+<a href="/react-native/docs/getting-started.html" target="_self">Docs</a></li>
+<li class=""><a href="/react-native/en/help.html" target="_self">Community</a></li>
+<li class=""><a href="/react-native/blog" target="_self">Blog</a></li><li class="navSearchWrapper reactNavSearchWrapper">
+<span class="algolia-autocomplete" style="position: relative; display: inline-block; direction: ltr;">
+<input type="text" id="search_input_react" placeholder="Search" class="aa-input" autocomplete="off" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-labelledby="search_input_react" aria-owns="algolia-autocomplete-listbox-0" dir="auto" style="position: relative; vertical-align: top;">
+<pre aria-hidden="true" style="position: absolute; visibility: hidden; white-space: pre; font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen, Ubuntu, Cantarell, &quot;Fira Sans&quot;, &quot;Droid Sans&quot;, &quot;Helvetica Neue&quot;, Arial, sans-serif; font-size: 14px; font-style: normal; font-variant: normal; font-weight: 300; word-spacing: 0px; letter-spacing: normal; text-indent: 0px; text-rendering: auto; text-transform: none;">
+</pre>
+<span class="aa-dropdown-menu" role="listbox" id="algolia-autocomplete-listbox-0" style="position: absolute; top: 100%; z-index: 100; display: none; left: 0px; right: auto;"><div class="aa-dataset-1"></div></span></span></li><li class=""><a href="https://github.com/facebook/react-native" target="_self">GitHub</a></li><li class=""><a href="https://reactjs.org/" target="_self">React</a></li></ul></nav></div></header></div></div>
+    );
+  }
+}
+export default nativ; 
